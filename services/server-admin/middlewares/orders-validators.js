ebaca32512f34bad012b283a6d673685735b098e
SERV-DRV <sderosa-2023220@kinal.edu.gt>
2026-02-26 18:29:49 -0600

    feat: Lógica de Validación y Disponibilidad de Mesas
    Crear validaciones robustas en la base de datos y en las rutas para verificar el estado de las mesas en tiempo real, bloqueando su asignación si ya están en uso para evitar duplicidad de ocupación

--- services/server-admin/middlewares/orders-validators.js
+++ services/server-admin/middlewares/orders-validators.js
@@ -1,9 +1,9 @@
 import { body } from "express-validator";
-import { validarCampos } from "./check-validators.js";
-import { validarJWT } from "./validate-jwt.js";
+import { checkValidators  } from "./check-validators.js";
+import { validateJWT } from "./validate-jwt.js";
 
 export const createOrderValidator = [
-    validarJWT,
+    validateJWT,
     body("table", "El ID de la mesa es obligatorio y debe ser válido").isMongoId(),
     body("restaurant", "El ID del restaurante es obligatorio").isMongoId(),
     body("items", "Los items son obligatorios y deben ser un arreglo").isArray({ min: 1 }),
@@ -11,19 +11,19 @@ export const createOrderValidator = [
     body("items.*.quantity", "La cantidad debe ser un número mayor a 0").isInt({ min: 1 }),
     body("items.*.price", "El precio del platillo es obligatorio").isNumeric(),
     body("items.*.modifiers", "Los modificadores deben ser un arreglo").optional().isArray(),
-    validarCampos
+    checkValidators 
 ];
 
 export const updateItemStatusValidator = [
-    validarJWT,
+    validateJWT,
     body("status", "El estado es obligatorio").notEmpty(),
     body("status", "Estado no válido").isIn(["EN_ESPERA", "EN_COCINA", "LISTO", "SERVIDO"]),
-    validarCampos
+    checkValidators 
 ];
 
 export const updateOrderStatusValidator = [
-    validarJWT,
+    validateJWT,
     body("status", "El estado es obligatorio").notEmpty(),
     body("status", "Estado no válido").isIn(["ABIERTA", "CERRADA", "CANCELADA"]),
-    validarCampos
+    checkValidators 
 ];
